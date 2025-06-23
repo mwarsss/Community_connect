566117981f651cf273d28e32d9cbb73699961d42
mwarsss <mwarewangepotinomudaishe@gmail.com>
2025-06-23 23:59:48 +0200

    Initial commit: Flask starter app with Docker prep

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,3 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev-key")
-
-
-# Import routes after creating the app to avoid circular imports
-# This is a common pattern in Flask applications to ensure that the app is created before routes are imported.
-# If routes are imported before the app is created, it can lead to circular import issues
-# because routes often need to access the app instance.
