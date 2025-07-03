61fa32376367f0e667da5c649e2d4ff75a9224f8
mwarsss <mwarewangepotinomudaishe@gmail.com>
2025-07-03 03:21:05 +0200

    Add SQLite oersistence with SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from .routes import register_routes
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 import os
 from dotenv import load_dotenv
@@ -7,5 +7,16 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev-key")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+# fmt: off
+from app.routes import register_routes  # Import after db is defined
+from app.models import Opportunity  # Import models to register them with SQLAlchemy
+# fmt: on
 register_routes(app)
+
+with app.app_context():
+    db.create_all()
