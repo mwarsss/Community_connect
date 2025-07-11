083aa897e63f5663dd7ead952befa2b9e7d55dea
mwarsss <mwarewangepotinomudaishe@gmail.com>
2025-07-11 20:12:41 +0200

    Add pagination to opportunity search

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,25 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-import os
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+import os
 
+
+# Load environment variables from .env file
 load_dotenv()
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev-key")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = "your_development_secret_key"  # use env var in production
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-# fmt: off
-from app.routes import register_routes  # Import after db is defined
-from app.models import Opportunity  # Import models to register them with SQLAlchemy
-# fmt: on
-register_routes(app)
 
-with app.app_context():
-    db.create_all()
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "dev")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
